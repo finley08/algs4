5034af62a6f5acc4a627644060417896eb7732ac
betrue <fazhang08@gmail.com>
2021-11-24 08:20:10 +0800

    refined

--- src/main/java/edu/princeton/cs/exercises/sort/Selection.java
+++ src/main/java/edu/princeton/cs/exercises/sort/Selection.java
@@ -7,6 +7,19 @@ public class Selection {
 
     public static void sort(Comparable[] a){
 
+        int N = a.length;
+
+        for(int i= 0; i<N; i++){
+            int min = i;
+            for(int j=i+1;j<N; j++){
+                if(less(a[j], a[min])){
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+            show(a);
+        }
+
     }
 
     private static boolean less(Comparable v,Comparable w){
@@ -21,9 +34,10 @@ public class Selection {
 
     private static void show(Comparable[] a){
         for(int i= 0 ; i<a.length ;i++){
-            StdOut.print("a["+i+"]->"+a[i]);
+            //StdOut.print("a["+i+"]->"+a[i] +" ");
+            StdOut.print(a[i] +" ");
         }
-        StdOut.print();
+        System.out.println("");
     }
 
     public static boolean isSorted(Comparable[] a){
@@ -36,9 +50,9 @@ public class Selection {
     }
 
     public static void main(String[] args){
-        int[] whitelist = In.readInts("../algs4-data/largeT.txt");
+        String[] ora = {"S","O","T","E"};
 
-
+        sort(ora);
     }
 
     
