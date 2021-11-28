4e2b087276eb1dd59a9d7fc3c19227e7f08d279c
betrue <fazhang08@gmail.com>
2021-11-28 22:02:34 +0800

    add shell sort

---
+++ src/main/java/edu/princeton/cs/exercises/sort/Shell.java
@@ -0,0 +1,58 @@
+package edu.princeton.cs.exercises.sort;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Shell {
+
+    public static void sort(Comparable[] a){
+        //将a[] 按升序排列
+        int N = a.length;
+        int h =1;
+        while(h<N/3){
+            h = 3*h +1;//1,4,13,40...
+        }
+
+        while(h >=1){
+            //将数组变为h有序
+            for(int i=h; i<N;i++){
+                for(int j=i; j>=h&&less(a[j], a[j-h]);j=j-h){
+                    exch(a, j, j-h);
+                }
+            }
+            h = h/3;
+        }
+
+
+    }
+
+
+
+
+    private static boolean less(Comparable v,Comparable w){
+        return v.compareTo(w)<0;
+    }
+    
+    private static void exch(Comparable[] a,int i, int j){
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static void show(Comparable[] a){
+        for(int i= 0 ; i<a.length ;i++){
+            //StdOut.print("a["+i+"]->"+a[i] +" ");
+            StdOut.print(a[i] +" ");
+        }
+        System.out.println("");
+    }
+
+    public static boolean isSorted(Comparable[] a){
+        for(int i = 1 ; i<a.length ;i++){
+            if(less(a[i], a[i-1])){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
